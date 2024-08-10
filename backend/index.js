58be6f4b98a8500ab21b68ba2130e8de68e36984
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-08-11 03:04:05 +0530

    Connect to the Database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import connectDB from './db.js';
 
 const app = express();
-
+connectDB();
 app.listen(3000,()=>{
     console.log(`server is running on Port 3000 !`);
     
