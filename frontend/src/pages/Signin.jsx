d463fc144862af0ff214b801a67659e6d1e9e808
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-08-11 21:25:48 +0530

    add redux persist

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -47,7 +47,7 @@ function Signin() {
     };
 
     return (
-        <div className="max-h-screen mt-20">
+        <div className="min-h-screen mt-20">
             <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
                 <div className="flex-1">
                     <Link
