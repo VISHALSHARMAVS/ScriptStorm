12174b90937d2f19ec5cfaa7efff6c876ce17e04
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-08-20 02:36:51 +0530

    cors issue fixed

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ import path from 'path'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 const app = express();
-app.use(cors({origin:'http://localhost:5173',credentials:true,methods:["PUT","GET","POST","DELETE"]}));
+const corsOptions = {
+    origin: 'https://scriptstorm-2.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  };
+  
+  app.use(cors(corsOptions));
 connectDB();
 app.use(express.json());
 app.use(cookieParser())
